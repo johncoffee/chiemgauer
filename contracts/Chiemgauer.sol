c36312e5e4fd9c2be948d07ce79a4f0109522525
unknown <tplenge@gmail.com>
2018-04-22 16:17:11 +0200

    Added deposit and withdrawal

--- contracts/Chiemgauer.sol
+++ contracts/Chiemgauer.sol
@@ -17,4 +17,23 @@ contract Chiemgauer is MintableToken, StandardBurnableToken {
 
    }
 
+    function unsafe_mint(address _to, uint256 _amount) public returns (bool) {
+        totalSupply_ = totalSupply_.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Mint(_to, _amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+    function unsafe_burn(address _who, uint256 _value) public {
+        require(_value <= balances[_who]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        balances[_who] = balances[_who].sub(_value);
+        totalSupply_ = totalSupply_.sub(_value);
+        emit Burn(_who, _value);
+        emit Transfer(_who, address(0), _value);
+    }
+
 }
