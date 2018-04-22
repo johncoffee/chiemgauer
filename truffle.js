9ef705510dbb6664bc521ebe0ed07f6e8cb54d4f
unknown <tplenge@gmail.com>
2018-04-22 14:39:30 +0200

    Started gateway contract

--- truffle.js
+++ truffle.js
@@ -2,9 +2,15 @@ module.exports = {
 	networks: {
 		// default
 		development: {
-		 host: "localhost",
-		 port: 7545,
-		 network_id: 5777,
+			 host: "localhost",
+			 port: 7545,
+			 network_id: 5777,
 		},
+        ropsten: {
+			host: "127.0.0.1",
+			port: 8545,
+			network_id: 3,
+			gas: 4700000
+        },
 	}
-}
+};
