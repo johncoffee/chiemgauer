580ff9c9c20b3b588e0e8f7b5e4eec69d56ea751
johncoffee <github2017@ragelse.dk>
2018-05-10 15:29:05 +0200

    remove useless arguments

--- migrations/2_deploy_Chiemgauer.js
+++ migrations/2_deploy_Chiemgauer.js
@@ -2,6 +2,6 @@ var contract = artifacts.require("./Chiemgauer.sol");
 
 module.exports = function(deployer) {
 
- deployer.deploy(contract, "0x7e0332fB5a513F340338bb33720b560e4D728752");
+ deployer.deploy(contract)
 
 };
